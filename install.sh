a4d3c1441f24a3cd64fcf724b77b25a7443eb702
kazi-amir <kaziah444@gmail.com>
2025-10-05 17:45:18 +0600

    changing more

---
+++ install.sh
@@ -0,0 +1,62 @@
+# Define color variables
+BOLD='\e[1m'
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[1;34m' # Bold Blue
+RESET='\e[0m'   # Reset all attributes
+
+
+sudo apt-get update 
+echo -e "${BOLD}Installing ${GREEN}snapd${RESET}:"
+sudo apt install snapd
+echo -e "${GREEN}snapd${RESET} installed Successfully"
+echo
+sleep 3
+
+echo -e "${BOLD}Updating the system:${RESET}"
+sudo apt update && sudo apt upgrade -y
+echo -e "${GREEN}updated Successfully${RESET}"
+sleep 3
+
+echo -e "${BOLD}Installing ${GREEN}build-essential${RESET}:"
+sudo apt-get install build-essential -y
+echo -e "${GREEN}build-essential${RESET} installed Successfully"
+echo
+sleep 3
+
+echo -e "${BOLD}Installing ${GREEN}vim${RESET}:"
+sudo apt-get install vim -y
+echo -e "${GREEN}vim${RESET} installed Successfully"
+echo
+sleep 3
+
+sudo apt-get install vim-gnome -y
+sudo apt install emacs -y
+sudo apt install gedit -y
+sudo apt install geany -y
+sudo apt install geany geany-plugins -y
+sudo apt install kate -y
+sudo apt-get install openjdk-17-jdk -y
+sudo apt-get install openjdk-17-jre -y
+sudo apt install python3 -y
+sudo apt install codeblocks -y
+sudo snap install --classic code -y
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
+
+sudo snap install --classic code
+sudo snap install --classic kotlin
+sudo snap install eclipse --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install clion --classic
+sudo snap install pycharm-community --classic
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install google-chrome-stable
+
+sudo apt update && sudo apt upgrade -y
--- run.sh
+++ run.sh
@@ -1,35 +1,13 @@
-sudo apt-get update
-sudo apt install snapd
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install build-essential -y
-sudo apt-get install vim -y
-sudo apt-get install vim-gnome -y
-sudo apt install emacs -y
-sudo apt install gedit -y
-sudo apt install geany -y
-sudo apt install geany geany-plugins -y
-sudo apt install kate -y
-sudo apt-get install openjdk-17-jdk -y
-sudo apt-get install openjdk-17-jre -y
-sudo apt install python3 -y
-sudo apt install codeblocks -y
-sudo snap install --classic code -y
+# Define color variables
+BOLD='\e[1m'
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[1;34m' # Bold Blue
+RESET='\e[0m'   # Reset all attributes
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text -y
-
-sudo snap install --classic code
-sudo snap install --classic kotlin
-sudo snap install eclipse --classic
-sudo snap install intellij-idea-community --classic
-sudo snap install clion --classic
-sudo snap install pycharm-community --classic
-
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt install google-chrome-stable
-
-sudo apt update && sudo apt upgrade -y
+echo -e "Hello ${GREEN}$USER${RESET}"
+echo
+echo "PC setup for this pc has been started. Please seat back and enjoy!"
+echo
+echo -e "${BOLD}Step - 1${RESET}: Installing necessary software"
+bash install.sh
